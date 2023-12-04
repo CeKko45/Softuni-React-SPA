1ac8cea1d807b740b9d9b30d94d37e4a8a2b3d70
CeKko45 <raketa4@abv.bg>
2023-12-04 15:30:03 +0200

    added path constants

---
+++ client/src/paths.js
@@ -0,0 +1,13 @@
+const Path = {
+  Home: "/",
+  Login: "/login",
+  Register: "/register",
+  Logout: "/logout",
+  CarList: "/cars",
+  CarCreate: "/cars/create",
+  CarDetails: "/cars/:carId",
+  CarEdit: "/cars/:carId/edit",
+  CarDelete: "/cars/:carId/delete",
+};
+
+export default Path;
